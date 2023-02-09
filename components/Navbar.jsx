1267c4378014a995f7e01f0461d17822c9030c32
JayG <jorgegil9706@gmail.com>
2023-02-09 01:43:23 -0500

    logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,15 +12,15 @@ const Navbar = () => (
   >
    
     <div className="absolute w-[50%] inset-0 gradiant-01"/>
-    <div className={`${styles.innerWidth} sm:pl-16 pl-6 sm:pr-16 pr-6 mx-auto flex justify-between gap-9 `}>
-      <img
-        src='/search.svg'
+    <div className={`${styles.innerWidth} sm:pl-16 pl-6 sm:pr-16 pr-6 mx-auto flex justify-between gap-9 overflow-hidden `}>
+      {/* <img
+        src='/logo2.png'
         alt='search'
-        className='w-[24px] h-[24px] object-contain'
-      />
+        className='h-20 w-20 sm:w-24 sm:h-24 lg:w-40 lg:h-40 object-contain'
+      /> */}
     
-      <h2 className="font-extrabold text-[25px] leading-[30px] text-white">
-        MJO take out
+      <h2 className="font-extrabold text-[25px] leading-[30px] text-[#CD9508]">
+        M.J.O Construction
       </h2>
       <img 
         src='/menu.svg'
